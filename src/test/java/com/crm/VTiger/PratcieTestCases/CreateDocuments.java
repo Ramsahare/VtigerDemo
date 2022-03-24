67c1e3583f6a32b14062d848453ffbe7faaaa248
Admin <Admin@VishnuPC>
2022-03-24 23:00:19 +0530

    Added Document Class

---
+++ src/test/java/com/crm/VTiger/PratcieTestCases/CreateDocuments.java
@@ -0,0 +1,13 @@
+package com.crm.VTiger.PratcieTestCases;
+
+import org.testng.annotations.Test;
+
+public class CreateDocuments {
+	
+	@Test
+	public void createDocuments() {
+		
+		System.out.println("Doc created");
+	}
+
+}
